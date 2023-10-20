52a92863dc6fcfcae4d4beab2cb09141cfcdbb6e
Jorge Oteiza <oteiza.jor@gmail.com>
2023-10-20 21:01:47 +0000

    second counter terminado

--- src/App.js
+++ src/App.js
@@ -1,34 +1,45 @@
 import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'
+import "font-awesome/css/font-awesome.min.css"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faClock } from '@fortawesome/free-regular-svg-icons';
 import './App.css';
+
+let interval = null;
 
 function SecondsCounter() {
   const [segundos, setSegundos] = useState(0);
 
   useEffect(() => {
-    const interval = setInterval(() => {
+    interval = setInterval(() => {
       setSegundos(segundos => segundos + 1);
     }, 1000);
 
     // Limpia el intervalo cuando el componente se desmonta
     return () => clearInterval(interval);
   }, []); // El segundo argumento del useEffect es un array vacío para que se ejecute solo una vez al montar el componente
+  
+  useEffect(() => {
+    if(segundos == 999999){
+      clearInterval(interval)
+    }
+  }, [segundos])
 
   // Limita el contador a 6 dígitos y agrega un símbolo de reloj
-  const formattedSeconds = `${('000000' + segundos).slice(-6)}`;
-
+  const formattedSeconds = segundos.toString().padStart(6, '0');
+console.log(segundos)
   return (
-    <div className='row'>
-      <div className="col">
+    <section style={{display: "flex"}}>
+      <div className="col col-2">
             <div className="card">
               <div className="card-body">
-                ⏰
+                <i className="fa fa-clock-o"></i>
               </div>
             </div>
-          </div>
-       {
-        formattedSeconds.toString().split('').map(digito => (
-          <div className="col" key={digito}>
+      </div>
+    <div className='row'>
+       {formattedSeconds.toString().split('').map((digito, index) => (
+          <div className="col col-2" key={digito + index}>
             <div className="card">
               <div className="card-body">
                 {digito}
@@ -38,6 +49,7 @@ function SecondsCounter() {
         ))
       }
     </div>
+    </section>
   );
 }
 
