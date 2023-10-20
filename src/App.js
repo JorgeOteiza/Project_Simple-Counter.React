365c7892b98c993288d89c33ecb238022e176baa
Jorge Oteiza <oteiza.jor@gmail.com>
2023-10-20 14:49:33 +0000

    avance estilos react-simple counter

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function SecondsCounter() {
   const [segundos, setSegundos] = useState(0);
 
   useEffect(() => {
-    const interval = setInterval (() => {
-      setSegundos(segundos => segundos +1);
-    }, 1000)
-  }, []);
+    const interval = setInterval(() => {
+      setSegundos(segundos => segundos + 1);
+    }, 1000);
+
+    // Limpia el intervalo cuando el componente se desmonta
+    return () => clearInterval(interval);
+  }, []); // El segundo argumento del useEffect es un array vacío para que se ejecute solo una vez al montar el componente
+
+  // Limita el contador a 6 dígitos y agrega un símbolo de reloj
+  const formattedSeconds = `⏰  ${('000000' + segundos).slice(-6)}`;
+
   return (
     <div>
-      <h1>Contador de segundos</h1>
-      <p>Segundos: {segundos}</p>
+      <h1>{formattedSeconds}</h1>
     </div>
   );
 }
-
 
 function App() {
   return (
